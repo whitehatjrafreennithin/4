85a257a197a122253b7dd040384298239903fbe1
whitehatjrafreennithin <78360402+whitehatjrafreennithin@users.noreply.github.com>
2021-07-16 18:08:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+rightWristX = 0;
+leftWristX = 0;
+noseX = 0;
+noseY = 0;
+difference = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+video.size(550,500)
+canvas = createCanvas(550, 418);
+canvas.position(560, 125);
+
+
+poseNet = ml5.poseNet(video, modelLoaded)
+poseNet.on("pose", gotPoses)
+
+}
+
+function modelLoaded() {
+    console.log("Model Loaded")
+}
+
+function gotPoses(results) {
+    if(results.length>0){
+        console.log(results)
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    }
+}
+
+function draw() {
+    background("coral")
+    fill("blue");
+stroke("blue");
+    text('Nithin', noseX, noseY);
+    textSize(difference);
+    document.getElementById("square_side").innerHTML = "Width and Height of the text will be " + difference + " Pixels"
+  
+}
